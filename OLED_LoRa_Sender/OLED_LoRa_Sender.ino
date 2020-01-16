b682254a1bdf84a5bd180e1a0b8f48e34817a314
LilyGO <31160602+LilyGO@users.noreply.github.com>
2020-01-16 17:54:50 +0800

    Update OLED_LoRa_Sender.ino

--- OLED_LoRa_Sender/OLED_LoRa_Sender.ino
+++ OLED_LoRa_Sender/OLED_LoRa_Sender.ino
@@ -8,7 +8,7 @@
 #define MISO    19   // GPIO19 -- SX1278's MISO
 #define MOSI    27   // GPIO27 -- SX1278's MOSI
 #define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
+#define RST     23   // GPIO14 -- SX1278's RESET
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND    868E6
 
